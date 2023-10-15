ddabdde44764679e0d3982e693711a37607b3973
mohammadreza.taherian <mohammadreza.taherian@partsotware.com>
2023-10-15 11:09:39 +0330

    new feature for multer and save file folder Date

--- utils/multerUtils.js
+++ utils/multerUtils.js
@@ -1,13 +1,32 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs-extra"); // Import fs-extra
+
+// Define the createSubdirectory function
+const createSubdirectory = () => {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = (now.getMonth() + 1).toString().padStart(2, "0"); // Months are zero-based
+  return `${year}-${month}`;
+};
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "../uploads")); // Save uploads in an "uploads" directory
+    const subdirectory = createSubdirectory();
+    const uploadDir = path.join(__dirname, "../uploads", subdirectory);
+
+    // Create the subdirectory if it doesn't exist
+    fs.ensureDir(uploadDir)
+      .then(() => {
+        cb(null, uploadDir);
+      })
+      .catch((err) => {
+        cb(err);
+      });
   },
   filename: (req, file, cb) => {
     const extension = path.extname(file.originalname);
-    cb(null, Date.now() + extension); // Rename the uploaded file with a timestamp
+    cb(null, Date.now() + extension);
   },
 });
 
