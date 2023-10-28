e56beab08690b1abb3fdb4e2d8af03ee29cc1de3
mohammadreza.taherian <mohammadreza.taherian@partsotware.com>
2023-10-28 11:11:59 +0330

    added folder config

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 //app.js
 const express = require("express");
 const { createConnection, getManager } = require("typeorm");
-const Users = require("./model/users");
-const OTP = require("./model/OTP");
-const Course = require("./model/Course");
-const Order = require("./model/Orders");
-const Category = require("./model/Category");
-const Session = require("./model/Session");
-const Cart = require("./model/Cart");
-const CartItems = require("./model/CartItems");
-
+const { setupDatabase ,configureSession} = require("./config/databaseConfig");
 const authRouter = require("./routes/auth/auth");
 const adminRouter = require("./routes/admin/admin");
 const cartRouter = require("./routes/shop/cart");
@@ -28,71 +20,13 @@ const PgSession = require("connect-pg-simple")(session);
 const dotenv = require("dotenv").config();
 const app = express();
 
-async function setupDatabase() {
-  try {
-    await createConnection({
-      type: "postgres",
-      host: process.env.DATABASE_URL,
-      port: process.env.PORT_PG_DB,
-      username: process.env.USERNAME_PG_DB,
-      password: process.env.PASSWORD_PG_DB,
-      database: process.env.DATABASE_PG_DB,
-      entities: [Users, OTP, Course, Order, Category, Session, Cart, CartItems],
-      synchronize: true,
-    });
-
-    console.log("Database connection established");
-  } catch (error) {
-    console.error("Error connecting to the database:", error);
-    throw error;
-  }
-}
-
 async function main() {
   try {
     await setupDatabase();
-
-    const userRepository = getManager().getRepository(Users);
-    const otpRepository = getManager().getRepository(OTP);
-    const courseRepository = getManager().getRepository(Course);
-    const orderRepository = getManager().getRepository(Order);
-    const categoryItemRepository = getManager().getRepository(Category);
-    const cartRepository = getManager().getRepository(Cart);
-    const cartItemsRepository = getManager().getRepository(CartItems);
-    const sessionRepository = getManager().getRepository(Session);
-    app.use(cookieParser());
-    app.use(
-      session({
-        store: new PgSession({
-          conObject: {
-            // Use your PostgreSQL connection settings here
-            user: process.env.USERNAME_PG_DB,
-            host: process.env.DATABASE_URL,
-            database: process.env.DATABASE_PG_DB,
-            password: process.env.PASSWORD_PG_DB,
-            port: process.env.PORT_PG_DB,
-          },
-          tableName: "session",
-        }),
-        secret: process.env.SESSION_SECRET,
-        resave: false,
-        saveUninitialized: true,
-        cookie: {
-          secure: false,
-          maxAge: 24 * 60 * 60 * 1000,
-        },
-      })
-    );
-    // app.use(
-    //   session({
-    //     secret: process.env.SESSION_SECRET,
-    //     resave: false,
-    //     saveUninitialized: false,
-    //   })
-    // );
-
+    configureSession(app);
     app.use(passport.initialize());
     app.use(passport.session());
+    app.use(cookieParser());
 
     app.use(bodyParser.urlencoded({ extended: false }));
 
