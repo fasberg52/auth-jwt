60606942c66997856573268986b48e479d9598ff
mohammadreza.taherian <mohammadreza.taherian@partsotware.com>
2023-11-01 14:26:08 +0330

    added editPart in part controller

--- controllers/part.js
+++ controllers/part.js
@@ -26,6 +26,45 @@ async function createPart(req, res) {
     res.status(201).json(savedPart);
   } catch (error) {
     console.error(`Error creating part: ${error}`);
-    res.status(500).json({ error: "An error occurred while creating the part." });
+    res
+      .status(500)
+      .json({ error: "An error occurred while creating the part." });
   }
 }
+
+async function editPart(req, res) {
+  try {
+    const { title, description, icon, videoPath } = req.body;
+    const partRepository = getManager().getRepository(Part);
+    const partId = req.params.id;
+
+    const existingPart = await partRepository.findOne({
+      where: { id: partId },
+    });
+
+    if (existingPart) {
+      existingPart.title = title;
+      existingPart.description = description;
+      existingPart.icon = icon;
+      existingPart.videoPath = videoPath;
+
+      // Save the updated part
+      existingPart.lastModified = new Date();
+      const updatedPart = await partRepository.save(existingPart);
+
+      res.json(updatedPart);
+    } else {
+      res.status(404).json({ error: "Part not found." });
+    }
+  } catch (error) {
+    console.error(`Error editing part: ${error}`);
+    res
+      .status(500)
+      .json({ error: "An error occurred while editing the part." });
+  }
+}
+
+module.exports = {
+  createPart,
+  editPart,
+};
