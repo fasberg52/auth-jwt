8b6c5f0122e45829b09365cb5cc3b9bd89424c78
mohamamdreza Taherian <taherian.1993@gmail.com>
2023-11-22 00:19:15 +0330

    sort in course Controller for allCourse

--- controllers/course.js
+++ controllers/course.js
@@ -7,35 +7,43 @@ async function getAllCourse(req, res) {
     const courseRepository = getManager().getRepository(Courses);
     const page = parseInt(req.query.page) || 1;
     const pageSize = parseInt(req.query.pageSize) || 10;
+    const sortBy = req.query.sortBy || 'id'; // Default to sorting by title
+    const sortOrder = req.query.sortOrder || 'ASC'; // Default to ascending order
 
     const offset = (page - 1) * pageSize;
 
-    const allCourses = await courseRepository
-      .createQueryBuilder("course")
-      .leftJoinAndSelect("course.category", "category") // Join with category
+    const [courses, total] = await courseRepository
+      .createQueryBuilder('course')
+      .leftJoinAndSelect('course.category', 'category') // Join with category
       .select([
-        "course.id",
-        "course.title",
-        "course.description",
-        "course.price",
-        "course.imageUrl",
-        "course.videoUrl",
-        "course.createdAt",
-        "course.lastModified",
+        'course.id',
+        'course.title',
+        'course.description',
+        'course.price',
+        'course.imageUrl',
+        'course.videoUrl',
+        'course.createdAt',
+        'course.lastModified',
       ])
-      .addSelect(["category.name"]) // Include category.name in the select
+      .addSelect(['category.name']) // Include category.name in the select
+      .orderBy(`course.${sortBy}`, sortOrder) // Add sorting
       .skip(offset)
       .take(pageSize)
-      .getMany();
+      .getManyAndCount();
 
-    res.json(allCourses);
+    res.json({
+      courses,
+      total,
+    });
   } catch (error) {
     console.log(error);
     res
       .status(500)
-      .json({ error: "An error occurred while creating the getAllCourse." });
+      .json({ error: 'An error occurred while creating the getAllCourse.' });
   }
 }
+
+
 
 async function getProductById(req, res) {
   try {
