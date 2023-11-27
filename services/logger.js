dbfcbfe3678a86c09b87c741817b8a0f53495a03
mohammadreza.taherian <mohammadreza.taherian@partsotware.com>
2023-11-27 15:52:10 +0330

    ignored logfile

--- services/logger.js
+++ services/logger.js
@@ -1,17 +1,25 @@
 //services//logger.js
-const winston = require('winston');
+const winston = require("winston");
 
 // Define the Winston logger configuration
 const logger = winston.createLogger({
-  level: 'info', // Set the default log level
+  level: "info", // Set the default log level
   format: winston.format.combine(
     winston.format.timestamp(),
     winston.format.simple()
   ),
   transports: [
     new winston.transports.Console(), // Log to the console
-    new winston.transports.File({ filename: 'logfile.log' }) // Log to a file
-  ]
+    new winston.transports.File({ filename: "logfile.log" }), // Log to a file
+  ],
 });
+
+logger.success = function (message, meta) {
+  this.log({
+    level: "success",
+    message,
+    meta,
+  });
+};
 
 module.exports = logger;
