ac22ebc8e4ff814af1ed5e556b279edad5c3431e
mohamamdreza Taherian <taherian.1993@gmail.com>
2023-12-06 18:03:34 +0330

    added cache router for delete cache

--- controllers/course.js
+++ controllers/course.js
@@ -91,52 +91,69 @@ async function getAllCourse(req, res) {
 }
 
 async function getCourseById(req, res) {
+  const cacheKey = "getCourseById";
   try {
-    const courseRepository = getManager().getRepository(Courses);
-    const courseId = req.params.courseId;
-
-    // Fetch the course with related chapters, parts, and category
-    const existingCourse = await courseRepository
-      .createQueryBuilder("course")
-      .leftJoin("course.category", "category")
-      .select([
-        "course.id",
-        "course.title",
-        "course.description",
-        "course.price",
-        "course.discountPrice",
-        "course.discountStart",
-        "course.discountExpiration",
-        "course.imageUrl",
-        "course.bannerUrl",
-        "course.videoUrl",
-        "course.createdAt",
-        "course.lastModified",
-      ])
-      .addSelect(["category.id", "category.name"])
-      .where("course.id = :courseId", { courseId })
-      .getOne();
-
-    if (existingCourse) {
-      // Convert dates to Jalali format
-      existingCourse.discountStart = jalaliMoment(
-        existingCourse.discountStart
-      ).format("YYYY/MM/DD HH:mm:ss");
-      existingCourse.discountExpiration = jalaliMoment(
-        existingCourse.discountExpiration
-      ).format("YYYY/MM/DD HH:mm:ss");
-      existingCourse.createdAt = jalaliMoment(existingCourse.createdAt).format(
-        "YYYY/MMMM/DD"
-      );
-      existingCourse.lastModified = jalaliMoment(
-        existingCourse.lastModified
-      ).format("YYYY/MMMM/DD");
-
-      logger.info(`getCourseById successful for courseId ${courseId}`);
-      res.json(existingCourse);
+    const cachedData = await cacheService.get(cacheKey);
+    console.log(`cacheKey >>> ${JSON.stringify(cachedData)}`);
+    if (cachedData !== undefined) {
+      console.log("Data found in cache. Returning cached data.");
+      const { existingCourse } = cachedData;
+      console.log(cacheKey);
+      // If data is found in the cache, return it
+      return res.json({ existingCourse });
     } else {
-      logger.info(`getCourseById failed for courseId ${courseId}`);
-      res.status(404).json({ error: "Course not found." });
+      console.log("Data NOT found in cache. Returning cached data.");
+
+      const courseRepository = getManager().getRepository(Courses);
+      const courseId = req.params.courseId;
+
+      // Fetch the course with related chapters, parts, and category
+      const existingCourse = await courseRepository
+        .createQueryBuilder("course")
+        .leftJoin("course.category", "category")
+        .select([
+          "course.id",
+          "course.title",
+          "course.description",
+          "course.price",
+          "course.discountPrice",
+          "course.discountStart",
+          "course.discountExpiration",
+          "course.imageUrl",
+          "course.bannerUrl",
+          "course.videoUrl",
+          "course.createdAt",
+          "course.lastModified",
+        ])
+        .addSelect(["category.id", "category.name"])
+        .where("course.id = :courseId", { courseId })
+        .getOne();
+
+      if (existingCourse) {
+        // Convert dates to Jalali format
+        existingCourse.discountStart = jalaliMoment(
+          existingCourse.discountStart
+        ).format("YYYY/MM/DD HH:mm:ss");
+        existingCourse.discountExpiration = jalaliMoment(
+          existingCourse.discountExpiration
+        ).format("YYYY/MM/DD HH:mm:ss");
+        existingCourse.createdAt = jalaliMoment(
+          existingCourse.createdAt
+        ).format("YYYY/MMMM/DD");
+        existingCourse.lastModified = jalaliMoment(
+          existingCourse.lastModified
+        ).format("YYYY/MMMM/DD");
+
+        const { price, ...cachedCourse } = existingCourse;
+
+        await cacheService.set(cacheKey, { cachedCourse }, 86400 * 1000);
+
+        logger.info(`getCourseById successful for courseId ${courseId}`);
+        res.json(existingCourse);
+      } else {
+        logger.info(`getCourseById failed for courseId ${courseId}`);
+        res.status(404).json({ error: "Course not found." });
+      }
     }
   } catch (error) {
     logger.error(`Error in getCourseById for courseId ${req.params.courseId}`, {
---
+++ routes/admin/cache.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+router.delete("clear-cache/:cacheKey", async (req, res) => {
+  const cacheKey = req.params.cacheKey;
+  try {
+    await cacheService.delete(cacheKey);
+    res.json({ message: `Cache cleared for key: ${cacheKey}` });
+  } catch (error) {
+    console.error(`Error clearing cache for key ${cacheKey}`, error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while clearing the cache." });
+  }
+});
+
+module.exports = router;
