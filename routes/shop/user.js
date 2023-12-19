a260ea25c797fbe740445131fb790fe6452e2919
mohammadreza.taherian <taherian.1993@gmail.com>
2023-12-19 15:27:53 +0330

    added user router

---
+++ routes/shop/user.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const { jwtAuthMiddleware } = require("../../middleware/jwtMiddleware");
+
+const router = express.Router();
+
+router.get("/profile", )
+
+module.exports = router;
