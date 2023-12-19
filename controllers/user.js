807eb2c1be045c80eaf47d57cb81ccc6712c3125
mohammadreza.taherian <taherian.1993@gmail.com>
2023-12-19 15:35:08 +0330

    added router user to config

--- controllers/user.js
+++ controllers/user.js
@@ -4,55 +4,49 @@ const logger = require("../services/logger");
 const moment = require("jalali-moment");
 const { convertToJalaliDate } = require("../services/jalaliService");
 
+const { verifyAndDecodeToken } = require("../utils/jwtUtils");
+
 async function getUserDataWithToken(req, res) {
-    try {
-      const userRepository = getManager().getRepository(User);
-      const token = req.headers.authorization;
-  
-      // Assuming you have a function to verify and decode the token
-      const decodedToken = verifyAndDecodeToken(token);
-  
-      if (!decodedToken || !decodedToken.userId) {
-        return res.status(401).json({ error: "Invalid or missing token" });
-      }
-  
-      const userId = decodedToken.userId;
-  
-      const existingUser = await userRepository.findOne(userId);
-  
-      if (existingUser) {
-        const userWithJalaliDates = {
-          id: existingUser.id,
-          firstName: existingUser.firstName,
-          lastName: existingUser.lastName,
-          phone: existingUser.phone,
-          role: existingUser.roles,
-          imageUrl: existingUser.imageUrl,
-          grade: existingUser.grade,
-          createdAt: moment(existingUser.createdAt).format("jYYYY/jMMMM/jDD"),
-          updatedAt: moment(existingUser.updatedAt).format("jYYYY/jMMMM/jDD"),
-          lastLogin: existingUser.lastLogin
-            ? moment(existingUser.lastLogin).format("jYYYY/jMMMM/jDD")
-            : null,
-        };
-  
-        res.json(userWithJalaliDates);
-      } else {
-        res.status(404).json({ error: "کاربری با این شماره پیدا نشد" });
-      }
-    } catch (error) {
-      res.status(500).json({ error: "Internal Server Error" });
+  try {
+    const userRepository = getManager().getRepository(User);
+    const token = req.headers.authorization;
+
+    // Assuming you have a function to verify and decode the token
+    const decodedToken = verifyAndDecodeToken(token);
+
+    if (!decodedToken || !decodedToken.userId) {
+      return res.status(401).json({ error: "Invalid or missing token" });
     }
+
+    const userId = decodedToken.userId;
+
+    const existingUser = await userRepository.findOne(userId);
+
+    if (existingUser) {
+      const userWithJalaliDates = {
+        id: existingUser.id,
+        firstName: existingUser.firstName,
+        lastName: existingUser.lastName,
+        phone: existingUser.phone,
+        role: existingUser.roles,
+        imageUrl: existingUser.imageUrl,
+        grade: existingUser.grade,
+        createdAt: moment(existingUser.createdAt).format("jYYYY/jMMMM/jDD"),
+        updatedAt: moment(existingUser.updatedAt).format("jYYYY/jMMMM/jDD"),
+        lastLogin: existingUser.lastLogin
+          ? moment(existingUser.lastLogin).format("jYYYY/jMMMM/jDD")
+          : null,
+      };
+
+      res.json(userWithJalaliDates);
+    } else {
+      res.status(404).json({ error: "کاربری با این شماره پیدا نشد" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
   }
-  
-  // Example function to verify and decode the token (you should implement this)
-  function verifyAndDecodeToken(token) {
-    // Your implementation to verify and decode the token
-    // This depends on the library or method you are using for authentication
-    // Return the decoded token or null if the verification fails
-  }
-  
-  module.exports = {
-    getUserDataWithToken,
-  };
-  
+}
+
+module.exports = {
+  getUserDataWithToken,
+};
