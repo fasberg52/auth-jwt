8368a2ee8a90fd4dc5fc646b40a78336e0497b8c
mohammadreza.taherian <taherian.1993@gmail.com>
2024-02-17 12:18:33 +0330

    added exam controller

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -2,21 +2,113 @@ const logger = require("../services/logger");
 const { quiz24Url } = require("../utils/axiosBaseUrl");
 async function registerUser(req, res) {
   try {
-    const response = await quiz24Url.post("/registerUser", req.body);
+    const userId = process.env.ADMIN_QUEZ24;
+    const requestBody = { ...req.body, userId };
+    const response = await quiz24Url.post("/registerUser", requestBody);
     return res.status(response.status).json(response.data);
   } catch (error) {
     logger.error(`Error in registerUser quiz24 ${error}`);
     res.status(500).json({ error: "Internal Server Error" });
   }
 }
-
+async function registerStudent(req, res) {
+  try {
+    const userId = process.env.ADMIN_QUEZ24;
+    const requestBody = { ...req.body, userId };
+    const response = await quiz24Url.post("/registerStudent", requestBody);
+    return res.status(response.status).json(response.data);
+  } catch (error) {
+    logger.error(`Error in registerStudent quiz24 ${error}`);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
 async function users(req, res) {
   try {
-    const response = await quiz24Url.post("/users", req.body);
+    const userId = process.env.ADMIN_QUEZ24;
+    const requestBody = { ...req.body, userId };
+    const response = await quiz24Url.post("/users", requestBody);
     return res.status(response.status).json(response.data);
   } catch (error) {
     logger.error(`Error in users quiz24 ${error}`);
     res.status(500).json({ error: "Internal Server Error" });
   }
 }
-module.exports = { registerUser, users };
+async function students(req, res) {
+  try {
+    const userId = process.env.ADMIN_QUEZ24;
+    const requestBody = { ...req.body, userId };
+    const response = await quiz24Url.post("/students", requestBody);
+    return res.status(response.status).json(response.data);
+  } catch (error) {
+    logger.error(`Error in students quiz24 ${error}`);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+
+async function examParticipation(req, res) {
+  try {
+    const userId = process.env.ADMIN_QUEZ24;
+    const requestBody = { ...req.body, userId };
+    const response = await quiz24Url.post("/examParticipation", requestBody);
+    return res.status(response.status).json(response.data);
+  } catch (error) {
+    logger.error(`Error in examParticipation quiz24 ${error}`);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+async function examResult(req, res) {
+  try {
+    const userId = process.env.ADMIN_QUEZ24;
+    const requestBody = { ...req.body, userId };
+    const response = await quiz24Url.post("/examResult", requestBody);
+    return res.status(response.status).json(response.data);
+  } catch (error) {
+    logger.error(`Error in examResult quiz24 ${error}`);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+async function answersheets(req, res) {
+  try {
+    const userId = process.env.ADMIN_QUEZ24;
+    const requestBody = { ...req.body, userId };
+    const response = await quiz24Url.post("/answersheets", requestBody);
+    return res.status(response.status).json(response.data);
+  } catch (error) {
+    logger.error(`Error in answersheets quiz24 ${error}`);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+async function exams(req, res) {
+  try {
+    const userId = process.env.ADMIN_QUEZ24;
+    const requestBody = { ...req.body, userId };
+    const response = await quiz24Url.post("/exams", requestBody);
+    return res.status(response.status).json(response.data);
+  } catch (error) {
+    logger.error(`Error in exams quiz24 ${error}`);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+
+async function exam(req, res) {
+  try {
+    const userId = process.env.ADMIN_QUEZ24;
+    const requestBody = { ...req.body, userId };
+    const response = await quiz24Url.post("/exam", requestBody);
+    return res.status(response.status).json(response.data);
+  } catch (error) {
+    logger.error(`Error in exam quiz24 ${error}`);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+module.exports = {
+  registerUser,
+  users,
+  registerStudent,
+  students,
+  examParticipation,
+  examResult,
+  answersheets,
+  exams,
+  exam,
+};
--- routes/shop/quiz.js
+++ routes/shop/quiz.js
@@ -1,6 +1,23 @@
 const express = require("express");
-const { registerUser } = require("../../controllers/quiz");
+const {
+  registerUser,
+  users,
+  registerStudent,
+  students,
+  examParticipation,
+  examResult,
+  answersheets,
+  exams,
+} = require("../../controllers/quiz");
 const router = express.Router();
 
 router.post("/registerUser", registerUser);
+router.post("/users", users);
+router.post("/registerStudent", registerStudent);
+router.post("/students", students);
+router.post("/examParticipation", examParticipation);
+router.post("/examResult", examResult);
+
+router.post("/answersheets", answersheets);
+router.post("/exams", exams);
 module.exports = router;
